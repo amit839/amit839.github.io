138dde85826a640056692180a34bb0101c48073f
amit839 <42930992+amit839@users.noreply.github.com>
2019-03-21 02:13:53 +0530

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -52,12 +52,12 @@ $(document).ready(function(){
       $('.js--wp-1').waypoint(function(direction){
           $('.js--wp-1').addClass('animated fadeInRightBig');
       },{
-          offset : '60%'
+          offset : '80%'
       });
       $('.js--wp-2').waypoint(function(direction){
           $('.js--wp-2').addClass('animated fadeInRightBig');
       },{
-          offset : '60%'
+          offset : '80%'
       });
       $('.js--wp-3').waypoint(function(direction){
           $('.js--wp-3').addClass('animated fadeInUp');
